7039b5a1124eeea77ad4a3966279c3988bde377c
Donor <donorfelita@msn.com>
2023-04-05 14:07:49 +0200

    Compare links with constant url

--- data_job_crawler/helpers/s3_helper.py
+++ data_job_crawler/helpers/s3_helper.py
@@ -1,3 +1,5 @@
+import re
+
 import ast
 import boto3
 from datetime import datetime, timedelta
@@ -11,20 +13,22 @@ class S3Helper:
 
     def __init__(self):
         self.today = datetime.now().strftime('%d-%m-%y')
-        self.bucket = "crawler-job-links"
         self.today_filename = self.get_filename_today()
-        self.yesterday_filename = self.get_filename_yesterday()
         self.today_filepath = self.get_today_filepath()
 
     def upload_new_links(self):
-        # Download latest file from S3
-        yesterday_links = self.extract_links_from_s3('yesterday')
+        # Download links from latest file on S3
+        latest_links = self.extract_links_from_s3('latest')
 
         # Get local file that we wish to add to S3
         today_links = self.extract_links_from_file()
 
-        # Substracts from local file links already present on S3 file
-        new_links = today_links - yesterday_links
+        # Prepare urls for comparison
+        today_links_constant = self.extract_constant_url(today_links)
+        latest_links_constant = self.extract_constant_url(latest_links)
+
+        # Compare
+        new_links = today_links_constant - latest_links_constant
 
         # Overwrite today's file with only new links
         with open(self.today_filepath, 'w') as f:
@@ -32,33 +36,46 @@ class S3Helper:
 
         self.upload_to_s3()
 
+    @staticmethod
+    def extract_constant_url(urls):
+        return set([re.search(r'.*(?=\?q=)', url).group(0) for url in urls])
+
+    def extract_links_from_s3(self, date='today'):
+        s3 = boto3.resource('s3')
+        bucket_name = "crawler-job-links"
+
+        if date == 'today':
+            filename = self.today_filename
+        elif date == 'latest':
+            filename = self.get_latest_modified(s3, bucket_name)
+
+        obj = s3.Object(bucket_name, key=filename)
+        links = obj.get()['Body'].read().decode('utf-8')
+        return ast.literal_eval(links)
+
+    @staticmethod
+    def get_latest_modified(s3, bucket_name):
+        bucket = s3.Bucket(bucket_name)
+        latest_file = None
+        last_modified_date = datetime(2022, 9, 1).replace(tzinfo=None)
+        for file in bucket.objects.all():
+            file_date = file.last_modified.replace(tzinfo=None)
+            if last_modified_date < file_date:
+                last_modified_date = file_date
+                latest_file = file
+        return latest_file.key
+
     def extract_links_from_file(self):
         with open(self.today_filepath, 'r') as f:
             links = f.read()
         return ast.literal_eval(links)
 
-    def extract_links_from_s3(self, date='today'):
-        s3 = boto3.resource('s3')
-        dated_filename = ''
-        if date == 'today':
-            dated_filename = self.today_filename
-        elif date == 'yesterday':
-            dated_filename = self.yesterday_filename
-
-        obj = s3.Object(self.bucket, key=dated_filename)
-        links = obj.get()['Body'].read().decode('utf-8')
-        return ast.literal_eval(links)
-
     def upload_to_s3(self):
         s3 = boto3.resource('s3')
-        s3.Bucket(self.bucket).upload_file(self.today_filepath, self.today_filename)
+        s3.Bucket("crawler-job-links").upload_file(self.today_filepath, self.today_filename)
 
     def get_filename_today(self):
         return f'wttj_links_{self.today}.txt'
-
-    def get_filename_yesterday(self):
-        yesterday = (datetime.today() - timedelta(days=1)).strftime('%d-%m-%y')
-        return f'wttj_links_{yesterday}.txt'
 
     def get_today_filepath(self):
         return Path(__file__).parent.parent / 'crawler' / 'data' / self.today_filename
